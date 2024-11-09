cc1b51c3880aaf6aeaed18d63e8f2a69b5d133bc
alexsandro alves de oliveira <140269838+alexsandro1234567@users.noreply.github.com>
2024-11-08 22:26:11 -0300

    Delete src/main/java/com/exemplo directory

--- src/main/java/com/exemplo/cidademod/util/BlockPosUtil.java
+++
@@ -1,7 +0,0 @@
-import net.minecraft.core.BlockPos;
-
-public class BlockPosUtil {
-    public static BlockPos create(int x, int y, int z) {
-        return new BlockPos(x, y, z);
-    }
-} 
--- src/main/java/com/exemplo/events/Requirement.java
+++
@@ -1,7 +0,0 @@
-package com.exemplo.cidademod.events;
-
-import net.minecraft.world.entity.player.Player;
-
-public interface Requirement {
-    boolean isMet(Player player);
-} 
